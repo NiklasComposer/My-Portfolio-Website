08081c36e93437f245caf89eadf4722d44acbcad
NiklasComposer <apfelknabniklas@gmail.com>
2025-08-26 21:04:34 +0200

    Vorübergehende Webpage angepasst

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello, this is my test React website!</h1>
-      <p>My Portfolio Website is currently under construction.</p>
-      <p>test4</p>
+      <h1>Thanks for checking out my website!</h1>
+      <h2>Unfortunately it is still in developement.</h2>
+      <p>Please check back later.</p>
     </div>
   )
 }
