93c67c82689ea907b84a9ed333f6e82d0abf7006
NiklasComposer <apfelknabniklas@gmail.com>
2025-08-26 20:48:50 +0200

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Hello, this is my test React website!</h1>
-      <p>Welcome to my portfolio test page.</p>
+      <p>My Portfolio Website is currently under construction.</p>
+      <p>test4</p>
     </div>
   )
 }
